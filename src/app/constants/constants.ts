938ae3d1b65487c7c35a69f3f82d8fb38c4f6425
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-02-02 01:42:48 +0000

    Update constants.ts

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,1 +1,5 @@
-export const TOKEN_CONTRACT_ADDRESS = "";
+import CustomChain from "../constants/chain";
+
+export const TOKEN_CONTRACT_ADDRESS = "0xB82DFfB5F5e5ECD44B91c64110EA68E9bD2Ee295";
+
+export const chainConst = CustomChain;
